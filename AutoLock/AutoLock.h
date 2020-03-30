bb77f0ac7631bce5d6ddfb01c57d9aa369db66dc
lemon4ex <netchen@vip.qq.com>
2020-03-30 14:02:49 +0800

    Initial Commit

---
+++ AutoLock/AutoLock.h
@@ -0,0 +1,9 @@
+//
+//  AutoLock.h
+//  AutoLock
+//
+//  Created by h4ck on 2020/3/30.
+//  Copyright (c) 2020 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ AutoLock/AutoLock.m
@@ -0,0 +1,15 @@
+//
+//  AutoLock.m
+//  AutoLock
+//
+//  Created by h4ck on 2020/3/30.
+//  Copyright (c) 2020 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "AutoLock.h"
+#import <CaptainHook/CaptainHook.h>
+
+CHConstructor // code block that runs immediately upon load
+{
+    NSLog(@"++++++++ AutoLock loaded ++++++++");
+}
